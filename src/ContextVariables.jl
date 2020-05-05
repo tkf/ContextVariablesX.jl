623f83c40511eeb4a9c7446913d92c783f75bcf5
Takafumi Arakaki <aka.tkf@gmail.com>
2020-05-05 00:26:33 -0700

    Initial implementation

--- src/ContextVariables.jl
+++ src/ContextVariables.jl
@@ -1,5 +1,40 @@
 module ContextVariables
 
-greet() = print("Hello World!")
+export context_storage, with_variables
+
+using Base.Meta: isexpr
+using IRTools
+using IRTools: @dynamo, IR, recurse!
+using UUIDs: UUID
+
+const CVKEY = UUID("30c48ab6-eb66-4e00-8274-c879a8246cdb")
+
+@dynamo function propagate_variables(args...)
+    ir = IR(args...)
+    ir === nothing && return
+    recurse!(ir)
+    return ir
+end
+
+# A hack to avoid "this intrinsic must be compiled to be called"
+propagate_variables(f::typeof(schedule), args...) = f(args...)
+
+function propagate_variables(::Type{<:Task}, f, args...)
+    vars = copy(task_local_storage(CVKEY))
+    function wrapper()
+        task_local_storage(CVKEY, vars)
+        f()
+    end
+    return Task(wrapper, args...)
+end
+
+function with_variables(f, kvs::Pair{Symbol}...)
+    task_local_storage(CVKEY, Dict(kvs)) do
+        propagate_variables(f)
+    end
+end
+
+context_storage(key) = task_local_storage(CVKEY)[key]
+context_storage(key, value) = task_local_storage(CVKEY)[key] = value
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,17 @@ using ContextVariables
 using Test
 
 @testset "ContextVariables.jl" begin
-    # Write your own tests here.
+    ok = Ref(0)
+    @sync @async begin
+        with_variables() do
+            context_storage(:mykey, "hello")
+            @test context_storage(:mykey) == "hello"
+            ok[] += 1
+            @async begin
+                @test context_storage(:mykey) == "hello"
+                ok[] += 1
+            end
+        end
+    end
+    @test ok[] == 2
 end
