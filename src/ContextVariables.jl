ebc2b550e2177c4ebc059b6974f1ea36309d7f4f
Takafumi Arakaki <aka.tkf@gmail.com>
2020-05-05 20:28:48 -0700

    Implement PEP 567-like interface

--- src/ContextVariables.jl
+++ src/ContextVariables.jl
@@ -1,13 +1,55 @@
 module ContextVariables
 
-export context_storage, with_variables
+export ContextVar, with_variables
 
 using Base.Meta: isexpr
-using IRTools
 using IRTools: @dynamo, IR, recurse!
-using UUIDs: UUID
+using UUIDs: UUID, uuid4
 
 const CVKEY = UUID("30c48ab6-eb66-4e00-8274-c879a8246cdb")
+
+struct ContextVar{T}
+    name::Symbol
+    key::UUID
+    has_default::Bool
+    default::T
+
+    ContextVar{T}(name::Symbol, default) where {T} = new{T}(name, uuid4(), true, default)
+    ContextVar{T}(name::Symbol) where {T} = new{T}(name, uuid4(), false)
+end
+
+ContextVar(name::Symbol, default) = ContextVar{typeof(default)}(name, default)
+ContextVar(name::Symbol) = ContextVar{Any}(name)
+
+Base.eltype(::Type{ContextVar{T}}) where {T} = T
+
+function Base.show(io::IO, var::ContextVar)
+    print(io, ContextVar)
+    if eltype(var) !== Any && !(var.has_default && typeof(var.default) === eltype(var))
+        print(io, '{', eltype(var), '}')
+    end
+    print(io, '(', repr(var.name))
+    if var.has_default
+        print(io, ", ")
+        show(io, var.default)
+    end
+    print(io, ')')
+end
+
+Base.getindex(var::ContextVar{T}) where {T} =
+    if var.has_default
+        get(task_local_storage(CVKEY), var.key, var.default)
+    else
+        task_local_storage(CVKEY)[var.key]
+    end::T
+
+function Base.setindex!(var::ContextVar{T}, value) where {T}
+    value = convert(T, value)
+    ctx = copy(task_local_storage(CVKEY))
+    ctx[var.key] = value
+    task_local_storage(CVKEY, ctx)
+    return var
+end
 
 @dynamo function propagate_variables(args...)
     ir = IR(args...)
@@ -29,12 +71,22 @@ function propagate_variables(::Type{<:Task}, f, args...)
 end
 
 function with_variables(f, kvs::Pair{Symbol}...)
-    task_local_storage(CVKEY, Dict(kvs)) do
+    with_variables_impl(kvs...) do
         propagate_variables(f)
     end
 end
 
-context_storage(key) = task_local_storage(CVKEY)[key]
-context_storage(key, value) = task_local_storage(CVKEY)[key] = value
+propagate_variables(::typeof(with_variables), args...) = with_variables_impl(args...)
+
+function with_variables_impl(f, kvs::Pair{<:ContextVar}...)
+    ctx0 = get(task_local_storage(), CVKEY, nothing)
+    if ctx0 === nothing
+        ctx = Dict{UUID,Any}()
+    else
+        ctx = copy(ctx0)
+    end
+    foldl(push!, (k.key => v for (k, v) in kvs), init = ctx)
+    return task_local_storage(f, CVKEY, ctx)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,60 @@
+module TestContextVariables
+
 using ContextVariables
+using ContextVariables: with_variables_impl
 using Test
 
-@testset "ContextVariables.jl" begin
+const cvar1 = ContextVar(:cvar1, 42)
+const cvar2 = ContextVar{Int}(:cvar2)
+const cvar3 = ContextVar(:cvar3)
+
+@testset "typed, w/ default" begin
     ok = Ref(0)
     @sync @async begin
         with_variables() do
-            context_storage(:mykey, "hello")
-            @test context_storage(:mykey) == "hello"
+            @test cvar1[] == 42
+            cvar1[] = 0
+            @test cvar1[] == 0
             ok[] += 1
             @async begin
-                @test context_storage(:mykey) == "hello"
+                @test cvar1[] == 0
                 ok[] += 1
             end
+            with_variables(cvar1 => 1) do
+                @test cvar1[] == 1
+                ok[] += 1
+            end
+            @test cvar1[] == 0
+            ok[] += 1
         end
     end
-    @test ok[] == 2
+    @test ok[] == 4
 end
+
+@testset "typed, w/o default" begin
+    with_variables_impl() do
+        @test_throws InexactError cvar2[] = 0.5
+        @test_throws KeyError cvar2[]
+        cvar2[] = 1.0
+        @test cvar2[] === 1
+    end
+end
+
+@testset "untyped, w/o default" begin
+    with_variables_impl() do
+        cvar3[] = 1
+        @test cvar3[] === 1
+        cvar3[] = 'a'
+        @test cvar3[] === 'a'
+    end
+end
+
+@testset "show" begin
+    @test endswith(sprint(show, ContextVar(:x, 42)), "ContextVar(:x, 42)")
+    @test endswith(sprint(show, ContextVar{Int}(:x)), "ContextVar{$Int}(:x)")
+    @test endswith(sprint(show, ContextVar(:x)), "ContextVar(:x)")
+    @test endswith(sprint(show, ContextVar{Union{Missing,Int64}}(:x, 1)),
+                   "ContextVar{Union{Missing, Int64}}(:x, 1)")
+end
+
+end  # module
